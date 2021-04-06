59c63ef407ab03806896135c5bbf97d5360498b5
Jose Rodriguez <isc.joserodriguez@gmail.com>
2021-04-05 20:58:28 -0600

    Todo App

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="card frame">
+        <Header />
+        <Form />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/container/App.js
@@ -0,0 +1,16 @@
+import '../css/App.css';
+function App() {
+  return (
+    <>
+      <header>
+        4 tareas por hacer
+      </header>
+      <form>
+        <input type="text" />
+        <button>Dame click</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
